89a1e327785c9308d32e2ea148b78e0a65bae1fc
javi-teaches <japsolo@gmail.com>
2022-10-24 09:15:44 -0500

    quedó el título listo

--- navbar.tsx
+++ navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function navbar() {
     return (
         <nav>
+            <h2>Título</h2>
             <ul>
                 <li>
                     <a href="">Item</a>
