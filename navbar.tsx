d0f64495a8c6057f38b1f963aba73023d7565c1f
javi-teaches <japsolo@gmail.com>
2022-10-24 09:02:14 -0500

    agregué el navbar

---
+++ navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Item</a>
+                </li>
+                <li>
+                    <a href="">Item</a>
+                </li>
+                <li>
+                    <a href="">Item</a>
+                </li>
+                <li>
+                    <a href="">Item</a>
+                </li>
+                <li>
+                    <a href="">Item</a>
+                </li>
+                <li>
+                    <a href="">Item</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
