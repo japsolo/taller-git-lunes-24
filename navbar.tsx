3e8d008b8de8d02bbd947a5634ad33a0f39d9de1
japsolo <jh.docente@gmail.com>
2022-10-24 09:15:09 -0500

    agregué el titulo

--- navbar.tsx
+++ navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function navbar() {
     return (
         <nav>
+            <h3>Un título</h3>
             <ul>
                 <li>
                     <a href="">Item</a>
